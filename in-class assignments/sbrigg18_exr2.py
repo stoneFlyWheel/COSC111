648b15cc42478ba75c1218088fdabbe1fb06f945
stoneflywheel <its.sage.briggs@gmail.com>
2026-08-26 11:55:51 -0400

    working 9 to 5

---
+++ in-class assignments/sbrigg18_exr2.py
@@ -0,0 +1,9 @@
+# givens
+initial_atoms = 8000000 # number of atoms at time zero
+half_life = 12 # half-life, in seconds
+elapsed_time = 50 # time elapsed, in seconds 
+
+# operations
+
+# how many half-lives have elapsed?
+half_lives_count = elapsed_time // half_life
