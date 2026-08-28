52ef6473a937fd9710597da7b3495b4124037448
stoneflywheel <its.sage.briggs@gmail.com>
2026-08-28 11:35:12 -0400

    fixed that thang

--- in-class assignments/sbrigg18_exr3.py
+++ in-class assignments/sbrigg18_exr3.py
@@ -1,7 +1,13 @@
-import maath
+import math
 miles = '123'
+kilometers = int(miles) * 1.61
 hours = 2
-average_speed = miles / hours
+average_speed = int(miles) / hours
+average_speed_km = kilometers / hours
+
 print('Trip distance:', miles, 'miles')
-print('Average speed:', average_speed, 'mph').
-print('Rounded speed:', round(average_speed, 1), 'mph') #prints an integer 
+print('Trip distance in km:', kilometers, 'km')
+print('Average speed:', average_speed, 'mph')
+print('Rounded speed:', round(average_speed)) #prints an integer 
+print('Average speed in km:', average_speed_km, 'kph')
+print('Rounded speed:', round(average_speed_km, 1))
