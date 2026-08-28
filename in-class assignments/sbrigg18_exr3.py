fd5ea0825c1108ef4aeae0b9ad71bb8bef1a19cd
stoneflywheel <its.sage.briggs@gmail.com>
2026-08-28 11:36:28 -0400

    added joke

--- in-class assignments/sbrigg18_exr3.py
+++ in-class assignments/sbrigg18_exr3.py
@@ -7,7 +7,8 @@ average_speed_km = kilometers / hours
 
 print('Trip distance:', miles, 'miles')
 print('Trip distance in km:', kilometers, 'km')
+# print('Average speed: 61.5 mph')
 print('Average speed:', average_speed, 'mph')
-print('Rounded speed:', round(average_speed)) #prints an integer 
+print('Rounded speed:', round(average_speed)) # prints an integer 
 print('Average speed in km:', average_speed_km, 'kph')
 print('Rounded speed:', round(average_speed_km, 1))
