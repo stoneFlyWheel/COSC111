c6c67578eaef90fad315f6e74c815ba93dc7cd0c
stoneflywheel <its.sage.briggs@gmail.com>
2026-08-28 11:30:36 -0400

    initial commit - base case with errors

---
+++ in-class assignments/sbrigg18_exr3.py
@@ -0,0 +1,7 @@
+import maath
+miles = '123'
+hours = 2
+average_speed = miles / hours
+print('Trip distance:', miles, 'miles')
+print('Average speed:', average_speed, 'mph').
+print('Rounded speed:', round(average_speed, 1), 'mph') #prints an integer 
