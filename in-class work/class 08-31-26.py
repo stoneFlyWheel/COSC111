ffa8ab84ac9f0b94e475ea82d1f68ea6c99f3b0c
stoneflywheel <its.sage.briggs@gmail.com>
2026-08-31 11:38:57 -0400

    initial commit

---
+++ in-class work/class 08-31-26.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle2 = turtle.Turtle()
+
+turtle2.color("blue")
+turtle2.pensize(5)
+turtle2.speed(1)
+turtle2.forward(100)
+turtle2.right(90)
+turtle2.forward(100)
+turtle2.right(90)
+turtle2.forward(100)
+turtle2.right(90)
+turtle2.forward(100)
+
+print("Hello World")
