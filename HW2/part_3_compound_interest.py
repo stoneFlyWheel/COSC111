e1b9006632ba45649c35b3635ed45ecf3bc045ae
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-02 11:48:51 -0400

    writing comments

--- HW2/part_3_compound_interest.py
+++ HW2/part_3_compound_interest.py
@@ -12,4 +12,9 @@ def total_interest(principal, annual_rate, years):
     return amount_owed(principal, annual_rate, years) - principal
 
 print("the total interest i paid in addition to my $25,000 is", round(total_interest(principal = 25000, annual_rate = 0.06, years = 5), 2), "dollars.")
+# the precondition for annual_rate is that it can't be more than 1! and most often values over 0.5 are way, way too high.
+# it also can't be negative, since that means the bank pays /you/ interest, which they hate
+# the postcondition for amount_owed is that it won't exceed principal * (1 + 0.5) ** years,
+# since the precondition is that annual_rate can't exceed 0.5.
 
+# it's better to have 
