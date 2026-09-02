53d705e4b13c1b8b2674ce01b5ce15aa1a7d04ac
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-02 11:42:52 -0400

    finish meat of assignment

--- HW2/part_3_compound_interest.py
+++ HW2/part_3_compound_interest.py
@@ -6,3 +6,10 @@ def amount_owed(principal, annual_rate, years):
     """
     return principal * (1 + annual_rate) ** years
 
+print("if i'm loaned $25,000, and have a yearly rate of 0.06 APR, then i owe", round(amount_owed(principal = 25000, annual_rate = 0.06, years = 5), 2), "dollars after 5 years.")
+
+def total_interest(principal, annual_rate, years):
+    return amount_owed(principal, annual_rate, years) - principal
+
+print("the total interest i paid in addition to my $25,000 is", round(total_interest(principal = 25000, annual_rate = 0.06, years = 5), 2), "dollars.")
+
