6370e214cee16d6f872c35c983dd0e4aa4b06a14
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-02 11:53:07 -0400

    initial commit

---
+++ in-class assignments/sbrigg18_exr4.py
@@ -0,0 +1,17 @@
+# part 1 ---------
+
+# define best function ever
+def print_greeting(name):
+    print("Hello,", name + "!", "It's so great to see you!")
+
+# call best function ever
+print_greeting("Sage")
+
+# part 2 ---------
+
+# define (actually the) best function ever
+def print_greeting(name, course):
+    print("Hello,", name + "!", "It's so great to see you! Welcome to", course + "!")
+
+# call the real best function ever
+print_greeting("Sage", "COSC111")
