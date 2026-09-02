1cfeaef9bc578c20a5b46c13faa3f7deb1c35cf0
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-02 11:25:11 -0400

    finish it

--- HW2/part_2_production_line.py
+++ HW2/part_2_production_line.py
@@ -2,16 +2,17 @@ shifts = ["alpha", "beta", "gamma"]
 shift_hours_worked = [10, 3, 8]
 unit_production_rate = 30 # 30 units per hour
 
+# first function
 def units_produced(units_per_hour, hours_worked):
     return units_per_hour * hours_worked
 
+# second function, load-bearing for 3rd
 def shift_report(shift_name, units_per_hour, hours_worked):
     units = units_produced(units_per_hour, hours_worked)
 
     return "shift " + shift_name + ": " + str(units) + " units produced"
 
-print(shift_report("laika", 15, 3))
-
+# da important function!
 def weekly_report():
     for index in range(len(shifts)):
         print(shift_report(shifts[index], unit_production_rate, shift_hours_worked[index]))
