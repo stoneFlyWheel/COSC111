1a0dbe63d5828f42fbebed6172a62323cd11913b
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-04 11:46:58 -0400

    initial commit

---
+++ in-class assignments/sbrigg18_exr5.py
@@ -0,0 +1,10 @@
+def square(num):
+    return num * num
+
+print(square(6))
+print(square(2))
+
+def sum_of_squares(num1, num2):
+    return square(num1) + square(num2)
+
+print(sum_of_squares(3, 4))
