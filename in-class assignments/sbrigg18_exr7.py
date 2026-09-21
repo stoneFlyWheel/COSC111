51af2385ad61a10253a518d771f00d7317e5970c
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-21 11:50:38 -0400

    editing

--- in-class assignments/sbrigg18_exr7.py
+++ in-class assignments/sbrigg18_exr7.py
@@ -8,13 +8,13 @@ def classify_number(n):
     message = ""
 
     if n == 0:
-        message = message + "zero"
+        message += "zero"
     else:
         if n > 0:
-            message = message +  "positive "
+            message += "positive "
         elif n < 0:
-            message = message +  "negative "
-        message = message + str(is_even(n))
+            message += "negative "
+        message += str(is_even(n))
 
     return message
 
