38e51b9e5ebdf856751b58ec1105d53f2055671c
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-21 11:41:48 -0400

    initial commit

---
+++ in-class assignments/sbrigg18_exr7.py
@@ -0,0 +1,8 @@
+def classify_number(n):
+      # your code here 
+
+# Test cases (no need to change anything below this line)
+print(classify_number(0))   # should print "zero"
+print(classify_number(4))   # "positive even"
+print(classify_number(-7))  # "negative odd"
+print(classify_number(-10)) # "negative even"
