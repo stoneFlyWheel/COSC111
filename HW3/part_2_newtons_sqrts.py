c875dd82e9d37ebd9061036b7d098e09ef281ccc
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-24 12:04:41 -0400

    part A done

--- HW3/part_2_newtons_sqrts.py
+++ HW3/part_2_newtons_sqrts.py
@@ -1,12 +1,13 @@
 # part A --------
 
-def newton_sqrt(x, a, tolerance=0.0001):
-    x2 = ((x + a) / x) / 2
+def newton_sqrt(a, x, tolerance=0.0001):
+    x2 = (x + (a / x)) / 2
 
     if abs(x2*x2 - a) < tolerance:
+        print(f"Approximate square root of {a} is {x2}")
         return x2
     else:
-        newton_sqrt(x2, a, tolerance=0.0001)
+        return newton_sqrt(a, x2, tolerance=0.0001)
 
 print(newton_sqrt(25, 1)) # approximately 5.0
 print(newton_sqrt(2, 1)) # approximately 1.4142...
