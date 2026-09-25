b31c3a2a148e7bb4f7c35ac20660faa6a8012bdb
stoneflywheel <its.sage.briggs@gmail.com>
2026-09-25 11:41:56 -0400

    done

---
+++ in-class assignments/sbrigg18_exr8.py
@@ -0,0 +1,32 @@
+import sys
+
+def is_even(n):
+    if n % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+def classify_number(n):
+    message = ""
+
+    if n == 0:
+        message += "zero"
+    else:
+        if n > 0:
+            message += "positive "
+        elif n < 0:
+            message += "negative "
+        message += is_even(n)
+
+    return message
+
+running = True
+while running:
+    num = input("please enter a number: ")
+    try:
+        num = float(num)
+        running = False
+    except:
+        print("Invalid input. Please enter a valid number.")
+
+print(classify_number(num))
